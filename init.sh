2c139e96211cade88be56b794cd1d00e480dc8d7
julien <j.repele@gmail.com>
2019-07-15 15:09:57 +0200

    Set sublime as default git editor

--- init.sh
+++ init.sh
@@ -14,6 +14,7 @@ git config --global alias.st status
 git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.lg "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+git config --global core.editor "sublime -n -w"
 
 # Install and configure OhMyZsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
