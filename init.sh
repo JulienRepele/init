a8af512e598d4f2b82d981c7266f9b49ef648aa8
Julien <julien@MacBook-Pro.local>
2021-05-28 10:41:32 +0200

    clean

--- init.sh
+++ init.sh
@@ -4,11 +4,13 @@ command xcode-select --install
 # Create dev directory
 cd ~
 mkdir ~/dev
+touch ~/.bash_profile
+echo "alias dev='cd ~/dev'" >> ~/.bash_profile 
 
 # Display hidden files
 defaults write com.apple.Finder AppleShowAllFiles true; killall Finder
 
-# Set acces right for Github SSH key
+# Set acces right for Github SSH key (if copied from another computer)
 # chmod 400 ~/.ssh/id_rsa
 
 # Make the keyboard cursor faster (effective after system reboot)
@@ -23,9 +25,9 @@ git config --global alias.br branch
 git config --global alias.lg "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 git config --global core.editor "sublime -n -w"
 
+# For people always typing gti instead of git
 touch ~/.bash_profile
-echo "alias gti='git'" >> ~/.bash_profile
-
+echo "alias gti='git'" >> ~/.bash_profile 
 
 # Install and configure OhMyZsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -43,16 +45,16 @@ echo "alias lint='git ls-files -m | xargs ktlint'" >> ~/.bash_profile
 # Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# Instal Sublime text and use it as git editor (instead of vim)
+# Instal Sublime Text and use it as git editor (instead of vim)
 brew install --cask sublime-text
 git config --global core.editor "subl -n -w"
 
-#Install popular softwares
+# Install popular softwares
 brew install --cask iterm2
 brew install --cask slack
 brew install --cask android-studio
 brew install --cask spotify
 brew install --cask intellij-idea-ce
 brew install --cask vlc
-brew install --cask transmission
+brew install --cask transmission # for dowloading Linux images
 brew install --cask adobe-creative-cloud
